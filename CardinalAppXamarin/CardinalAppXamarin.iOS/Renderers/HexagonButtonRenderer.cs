3f1f3109e9455fd6965fe50425b6628ed5aa15c4
Ryan Rauch <rauch.ryan@gmail.com>
2018-06-24 15:22:20 -0500

    .

---
+++ CardinalAppXamarin/CardinalAppXamarin.iOS/Renderers/HexagonButtonRenderer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using CardinalAppXamarin.Controls;
+using CardinalAppXamarin.iOS.Renderers;
+using Foundation;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(HexagonButton), typeof(HexagonButtonRenderer))]
+namespace CardinalAppXamarin.iOS.Renderers
+{
+    public class HexagonButtonRenderer : ButtonRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Button> e)
+        {
+            base.OnElementChanged(e);
+        }
+
+        protected override void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            base.OnElementPropertyChanged(sender, e);
+        }
+
+        private void Paint(HexagonButton view)
+        {
+            //this.Layer.CornerRadius = (float)view.CustomBorderRadius;
+            //this.Layer.BorderColor = view.CustomBorderColor.ToCGColor();
+            //this.Layer.BackgroundColor = view.CustomBackgroundColor.ToCGColor();
+            //this.Layer.BorderWidth = (int)view.CustomBorderWidth;
+        }
+    }
+}
---
+++ CardinalAppXamarin/CardinalAppXamarin/Controls/HexagonButton.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace CardinalAppXamarin.Controls
+{
+    public class HexagonButton : Button
+    {
+        public static readonly BindableProperty PointedTopProperty =
+                            BindableProperty.Create(nameof(PointedTop),
+                                                    typeof(bool),
+                                                    typeof(HexagonButton),
+                                                    true);
+        public bool PointedTop
+        {
+            get { return (bool)GetValue(PointedTopProperty); }
+            set { SetValue(PointedTopProperty, value); }
+        }
+    }
+}
